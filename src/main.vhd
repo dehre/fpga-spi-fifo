87fd346b6fd57878015f967dc71ac7c5fb31e178
Loris Guerra <git.dehre@gmail.com>
2024-11-03 13:49:45 +0100

    rename pin constraints for clarity

--- src/main.vhd
+++ src/main.vhd
@@ -10,42 +10,23 @@ use ieee.numeric_std.all;
 
 entity SpiFollower is
   port (
-    -- DEBUGGING OUTPUTS
-    io_pmod_7    : out  std_logic; 
-    io_pmod_8    : out  std_logic;
-    io_pmod_9    : out  std_logic;
+    -- Debugging Outputs
+    o_debug_a    : out  std_logic; 
+    o_debug_b    : out  std_logic;
+    o_debug_c    : out  std_logic;
 
     -- Control/Data Signals
-
-    -- i_Rst_L    : in  std_logic;    -- FPGA Reset, active low
-    io_pmod_10    : in  std_logic;  -- FPGA Reset, active low
-
+    i_rst_n    : in  std_logic;    -- FPGA Reset, active low
     i_clk      : in  std_logic;    -- FPGA Clock
 
     -- SPI Interface
-
-    -- i_SPI_Clk  : in  std_logic;
-    io_pmod_1  : in  std_logic; -- SPI Clock
-
-    -- o_SPI_MISO : out std_logic     -- Master In, Slave Out
-    io_pmod_2 : out std_logic;     -- Master In, Slave Out
-
-    -- i_SPI_MOSI : in  std_logic;    -- Master Out, Slave In
-    io_pmod_3 : in  std_logic;    -- Master Out, Slave In
-
-    -- i_SPI_CS_n : in  std_logic;    -- Chip Select, active low
-    io_pmod_4 : in  std_logic);    -- Slave Select, active low
+    i_spi_clk  : in  std_logic;
+    o_spi_miso : out std_logic;     -- Master In, Slave Out
+    i_spi_mosi : in  std_logic;     -- Master Out, Slave In
+    i_spi_cs_n : in  std_logic);    -- Chip Select, active low
 end entity;
 
 architecture RTL of SpiFollower is
-
-  -- Wires for renaming pmod pins
-  -- TODO LORIS: maybe rename pin-constraints directly
-  signal wi_Rst_L: std_logic;
-  signal wi_SPI_Clk: std_logic;
-  signal wo_SPI_MISO: std_logic;
-  signal wi_SPI_MOSI: std_logic;
-  signal wi_SPI_CS_n: std_logic;
 
   -- Internal signals
   signal w_RX_DV    : std_logic := '0';
@@ -58,12 +39,6 @@ architecture RTL of SpiFollower is
 
 begin
 
-  -- Renaming inputs/outputs for clarity
-  wi_Rst_L    <= io_pmod_10;
-  wi_SPI_Clk  <= io_pmod_1;
-  io_pmod_2 <= wo_SPI_MISO;
-  wi_SPI_MOSI <= io_pmod_3;
-  wi_SPI_CS_n <= io_pmod_4;
 
   process (i_clk, r_clk_half)
   begin
@@ -76,16 +51,16 @@ begin
   SpiSlaveInstance: entity work.SPI_Slave
     generic map (SPI_MODE => 0)
     port map (
-      o_debug_a => io_pmod_7,
-      o_debug_b => io_pmod_8,
-      o_debug_c => io_pmod_9,
+      o_debug_a => o_debug_a,
+      o_debug_b => o_debug_b,
+      o_debug_c => o_debug_c,
 
       i_Clk      => i_clk,
-      i_Rst_L    => wi_Rst_L,
-      i_SPI_Clk  => wi_SPI_Clk,
-      o_SPI_MISO => wo_SPI_MISO,
-      i_SPI_MOSI => wi_SPI_MOSI,
-      i_SPI_CS_n => wi_SPI_CS_n,
+      i_Rst_L    => i_rst_n,
+      i_SPI_Clk  => i_spi_clk,
+      o_SPI_MISO => o_spi_miso,
+      i_SPI_MOSI => i_spi_mosi,
+      i_SPI_CS_n => i_spi_cs_n,
       o_RX_DV    => w_RX_DV,
       o_RX_Byte  => w_RX_Byte,
       i_TX_DV    => w_TX_DV,
