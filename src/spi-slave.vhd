ff75e957883a96e13df4eefb4ce15e1e2d7a019f
Loris Guerra <git.dehre@gmail.com>
2024-11-03 14:33:32 +0100

    spi: process received bits

--- src/spi-slave.vhd
+++ src/spi-slave.vhd
@@ -51,14 +51,8 @@ end entity;
 architecture RTL of SPISlave is
 
   -- RECEIVE SIGNALS
-  signal r_RX_Bit_Count : unsigned(2 downto 0) := (others => '0');
-  signal r_Temp_RX_Byte : std_logic_vector(7 downto 0) := (others => '0');
-  signal r_RX_Byte : std_logic_vector(7 downto 0) := (others => '0');
-
-  -- TODO LORIS: this somehow adds some delay to setting RX_DV to 1,
-  -- with the goal of setting o_RX_DV up for one clock cycle.
-  -- Look at the paper you wrote, r3 isn't needed anymore.
-  signal r_RX_Done, r2_RX_Done, r3_RX_Done : std_logic := '0';
+  signal r_RX_Byte : std_logic_vector(7 downto 0);
+  signal r_RX_Done : std_logic;
 
   -- TRANSMIT SIGNALS
   signal r_TX_Byte : std_logic_vector(7 downto 0);
@@ -69,57 +63,24 @@ begin
   -- RECEIVING BLOCKS
   --
 
-  -- Get SPI Byte in SPI Clock Domain
-  process (i_SPI_Clk, i_SPI_CS_n)
+  process(i_SPI_CS_n, i_SPI_Clk)
+    variable v_RX_Bit_Count : integer := -1;
   begin
+
     if i_SPI_CS_n = '1' then
-      r_RX_Bit_Count <= (others => '0');
-      r_RX_Done <= '0';
+      v_RX_Bit_Count := -1;
     elsif rising_edge(i_SPI_Clk) then
-      r_RX_Bit_Count <= r_RX_Bit_Count + 1; -- Rolls back to '000' eventually
-      r_Temp_RX_Byte <= r_Temp_RX_Byte(6 downto 0) & i_SPI_MOSI;
-
-      if r_RX_Bit_Count = "111" then
-        r_RX_Done <= '1';
-        r_RX_Byte <= r_Temp_RX_Byte(6 downto 0) & i_SPI_MOSI;
-
-      -- TODO LORIS: just else?
-      -- it seems r_RX_Done is kept high for a few cycles just to make sure we
-      -- can catch it in the FPGA-clock domain and correctly pulse o_RX_DV when done.
-      -- TODO LORIS: with the alternative you wrote on the paper, you reset
-      -- r_RX_Done in the other block, not here (but write a comment here above when
-      -- you set it to 1).
-      elsif r_RX_Bit_Count = "010" then
-        r_RX_Done <= '0';
-      end if;
+      v_RX_Bit_Count := v_RX_Bit_Count + 1;
     end if;
-  end process;
 
-  -- Cross from SPI Clock Domain to FPGA clock domain.
-  -- Goal: pulse o_RX_DV when done receiving.
-  -- The additional r2_RX_Done and r3_RX_Done add a two *FPGA-clock* delay
-  -- to setting o_RX_DV to 1: when r2 goes high, RX_DV goes high, when r3 goes
-  -- high, RX_DV goes low again.
-  -- TODO LORIS: check better, probably two registers are for crossing
-  -- clock-domains, not for rising and falling edge.
-  process (i_Clk, i_Rst_L)
-  begin
-    if i_Rst_L = '0' then
-      r2_RX_Done <= '0';
-      r3_RX_Done <= '0';
-      o_RX_DV <= '0';
-      o_RX_Byte <= (others => '0');
-    elsif rising_edge(i_Clk) then
-      r2_RX_Done <= r_RX_Done;
-      r3_RX_Done <= r2_RX_Done;
-
-      if r3_RX_Done = '0' and r2_RX_Done = '1' then
-        o_RX_DV <= '1';  -- Pulse Data Valid 1 clock cycle
-        o_RX_Byte <= r_RX_Byte;
-      else
-        o_RX_DV <= '0';
-      end if;
+    if v_RX_Bit_Count = 7 then
+      r_RX_Done <= '1';
+    elsif v_RX_Bit_Count = 2 then -- TODO LORIS: needed? otherwise just reset
+      r_RX_Done <= '0';
     end if;
+
+    r_RX_Byte(v_RX_Bit_Count) <= i_SPI_MOSI;
+
   end process;
 
   --
