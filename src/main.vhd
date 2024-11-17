4b07dc2054ecacccc870dcdeddf58463e9021ddc
Loris Guerra <git.dehre@gmail.com>
2024-11-17 10:58:14 +0100

    main: adapt to new spi-slave module

--- src/main.vhd
+++ src/main.vhd
@@ -2,15 +2,15 @@ library ieee;
 use ieee.std_logic_1164.all;
 use ieee.numeric_std.all;
 
-entity SpiFollower is
+entity SPIFollower is
   port (
     -- Debugging Outputs
-    o_debug_a    : out  std_logic; 
-    o_debug_b    : out  std_logic;
-    o_debug_c    : out  std_logic;
+    o_debug_a  : out  std_logic; 
+    o_debug_b  : out  std_logic;
+    o_debug_c  : out  std_logic;
 
     -- Control/Data Signals
-    i_rst_n    : in  std_logic;    -- FPGA Reset, active low
+    i_rst      : in  std_logic;    -- FPGA Reset
     i_clk      : in  std_logic;    -- FPGA Clock
 
     -- SPI Interface
@@ -20,54 +20,48 @@ entity SpiFollower is
     i_spi_cs_n : in  std_logic);    -- Chip Select, active low
 end entity;
 
-architecture RTL of SpiFollower is
+architecture RTL of SPIFollower is
 
-  -- Internal signals
-  signal w_rx_dv    : std_logic := '0';
-  signal w_rx_byte  : std_logic_vector(7 downto 0) := (others => '0');
-  signal w_tx_dv    : std_logic := '0';
-  signal w_tx_byte  : std_logic_vector(7 downto 0) := (others => '0');
-
-  -- Debugging
-  signal r_clk_half : std_logic;
+  constant WORD_SIZE : integer := 8; -- SPI word size, 8 bits
+  signal r_din       : std_logic_vector(WORD_SIZE-1 downto 0); -- Data to send to master
+  signal r_din_vld   : std_logic;    -- Data valid signal for SPI slave
+  signal w_din_rdy   : std_logic;    -- Ready signal from SPI slave
+  signal w_dout      : std_logic_vector(WORD_SIZE-1 downto 0); -- Data received from master
+  signal w_dout_vld  : std_logic;    -- Data valid signal from SPI slave
 
 begin
 
-  -- Debugging
-  process (i_clk, r_clk_half)
-  begin
-    if rising_edge(i_clk) then
-      r_clk_half <= not r_clk_half;
-    end if;
-  end process;
-
-  -- Instantiate the SPISlave component
-  SpiSlaveInstance: entity work.SPISlave
+  SPISlaveInstance : entity work.SPISlave
+    generic map (WORD_SIZE => WORD_SIZE)
     port map (
-      o_debug_a => o_debug_a,
-      o_debug_b => o_debug_b,
-      o_debug_c => o_debug_c,
-
       i_clk      => i_clk,
-      i_rst_n    => i_rst_n,
+      i_rst      => i_rst,
       i_spi_clk  => i_spi_clk,
-      o_spi_miso => o_spi_miso,
-      i_spi_mosi => i_spi_mosi,
       i_spi_cs_n => i_spi_cs_n,
-      o_rx_dv    => w_rx_dv,
-      o_rx_byte  => w_rx_byte,
-      i_tx_dv    => w_tx_dv,
-      i_tx_byte  => w_tx_byte);
+      i_spi_mosi => i_spi_mosi,
+      o_spi_miso => o_spi_miso,
+      i_din      => r_din,
+      i_din_vld  => r_din_vld,
+      o_din_rdy  => w_din_rdy,
+      o_dout     => w_dout,
+      o_dout_vld => w_dout_vld);
 
     process(i_clk)
     begin
       if rising_edge(i_clk) then
-        if w_rx_dv = '1' then
-          -- w_tx_byte <= w_rx_byte;
-          w_tx_byte <= "10101010";
-          w_tx_dv   <= '1';
+        if i_rst = '1' then
+          r_din     <= (others => '0');
+          r_din_vld <= '0';
         else
-          w_tx_dv   <= '0';
+          -- When new data is received from master,
+          -- load it back into din to send it on the next transaction
+          if w_dout_vld = '1' then
+            r_din <= w_dout;
+            r_din_vld <= '1';
+          elsif w_din_rdy = '1' then
+            -- Clear r_din_vld once SPI slave is ready to accept new data
+            r_din_vld <= '0';
+          end if;
         end if;
       end if;
     end process;
