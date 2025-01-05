c688521467b60f60dbc0c3e4760a1546379248ee
Loris Guerra <git.dehre@gmail.com>
2025-01-05 14:21:34 +0100

    number-to-bcd: use double-dabble algorithm

--- src/number-to-bcd.vhd
+++ src/number-to-bcd.vhd
@@ -1,10 +1,15 @@
 -- Converts a natural number (0 to 99) into its two-digit BCD representation.
--- For inputs outside the valid range, the module outputs 0xFF as a fail-safe.
 -- 
--- TODO LORIS:
--- Warning: Found 1 combinational loops!
--- @W: BN137 :"c:\users\marce\desktop\new folder\src\number-to-bcd.vhd":26:16:26:28
--- Found combinational loop during mapping at net NumberToBCDInstance.mult1_un54_sum_i[5]
+-- The conversion is implemented using the "double-dabble" algorithm.
+-- It has been adapted from:
+-- https://vhdlguru.blogspot.com/2010/04/8-bit-binary-to-bcd-converter-double.html
+-- 
+-- A simpler implementations, such as:
+-- ```
+-- o_ones_bcd <= std_logic_vector(to_unsigned(i_number mod 10, o_ones_bcd'length));
+-- o_tens_bcd <= std_logic_vector(to_unsigned(i_number / 10  , o_tens_bcd'length));
+-- ```
+-- was discarded to avoid combinational loops during synthesis.
 
 library ieee;
 use ieee.std_logic_1164.all;
@@ -18,12 +23,42 @@ entity NumberToBCD is
 end entity;
 
 architecture RTL of NumberToBCD is
-  signal w_tens : natural range 0 to 15;
-  signal w_ones : natural range 0 to 15;
-begin
-  w_ones <= i_number mod 10 when i_number <= 99 else 15;
-  w_tens <= i_number / 10   when i_number <= 99 else 15;
 
-  o_ones_bcd <= std_logic_vector(to_unsigned(w_ones, o_ones_bcd'length));
-  o_tens_bcd <= std_logic_vector(to_unsigned(w_tens, o_tens_bcd'length));
+  signal w_binary : unsigned(7 downto 0);
+  signal w_bcd : unsigned(7 downto 0);
+
+  function f_binary_to_bcd (w_binary : unsigned(7 downto 0))
+  return unsigned is
+    variable v_idx : integer := 0;
+    variable v_bcd : unsigned(7 downto 0) := (others => '0'); -- 2-digits BCD
+  begin
+    for v_idx in 7 downto 1 loop
+      -- left shift the bits
+      v_bcd := v_bcd(6 downto 0) & w_binary(v_idx);
+
+      -- adjust ones-digit if greater than 4
+      if v_bcd(3 downto 0) > 4 then
+        v_bcd(3 downto 0) := v_bcd(3 downto 0) + 3;
+      end if;
+
+      -- adjust tens-digit if greater than 4
+      if v_bcd(7 downto 4) > 4 then
+        v_bcd(7 downto 4) := v_bcd(7 downto 4) + 3;
+      end if;
+    end loop;
+
+    -- final shift for the last bit
+    v_bcd := v_bcd(6 downto 0) & w_binary(0);
+
+    -- return the result
+    return v_bcd;
+  end function;
+
+begin
+
+  w_binary   <= to_unsigned(i_number, 8);
+  w_bcd      <= f_binary_to_bcd(w_binary);
+  o_ones_bcd <= std_logic_vector(w_bcd(3 downto 0));
+  o_tens_bcd <= std_logic_vector(w_bcd(7 downto 4));
+
 end architecture;
